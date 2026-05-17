dc55e9b956e83cdc72ae2a4a146e127b8fafa1ef
Violet <viob663@gmail.com>
2026-05-17 10:00:45 +0300

    first commit

---
+++ pract.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// const answer = prompt("Are you 18?", "");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt("ВВедите ваше имя", "");
+// answers[1] = prompt("ВВедите вашу фамилию", "");
+// answers[2] = +prompt("ВВедите ваш возраст", "");
+
+// document.write(answers);
+
+// const category = "toys";
+
+// console.log(`https://someurl/${category}/5`);
+
+// const user = "Violet";
+
+// alert(`Hello, ${user}!`);
+
+// let incr = 10,
+//   decr = 10;
+
+// // incr++;
+// // decr--;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2 * 4 === "8");
+
+const isOpened = true,
+  isClosed = false;
+
+console.log(isOpened && !isClosed);
